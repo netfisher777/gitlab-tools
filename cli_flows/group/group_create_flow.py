2f664d0f97351cb21dc973306cac6ae65ee07fa7
netfisher777 <netfisher777@gmail.com>
2020-01-21 10:05:55 +0300

    load available groups and print

--- cli_flows/group/group_create_flow.py
+++ cli_flows/group/group_create_flow.py
@@ -1,5 +1,7 @@
 from cli_flows.flow_base import FlowBase
 from gitlab_api.gitlab_adapter import GitlabAdapter
+from gitlab_api.gitlab_group import GitlabGroup
+from typing import Dict
 
 
 class GroupCreateFlow(FlowBase):
@@ -7,28 +9,32 @@ class GroupCreateFlow(FlowBase):
         super().__init__()
         self.__group_name = None
         self.__group_alias = None
-        self.__available_repositories = None
+        self.__available_groups_dict: Dict[int, GitlabGroup] = None
+        self.__current_repositories_dict = None
         self.__gitlab_adapter: GitlabAdapter = None
 
     def start(self):
         super().start()
         print('Executing create new group command')
+        self.__init_gitlab_adapter()
         self.__load_initial_data()
         self.__group_name = input('Enter new group name: ')
         self.__group_alias = input('Enter new group alias: ')
-        print('Choose repositories to add in new group:')
-        self.__print_available_repositories()
-
-    def __load_initial_data(self):
-        self.__init_gitlab_adapter()
-        self.__init_available_repositories()
+        print('Choose repositories to add below:')
+        self.__add_repositories_loop()
 
     def __init_gitlab_adapter(self):
         self.__gitlab_adapter = GitlabAdapter.from_connection_settings()
         self.__gitlab_adapter.connect()
 
-    def __init_available_repositories(self):
-        self.__gitlab_adapter.get_available_groups()
+    def __add_repositories_loop(self):
+        print('Choose group to add repositories from:')
+        self.__print_available_groups_dict()
 
-    def __print_available_repositories(self):
-        pass
+    def __load_initial_data(self):
+        groups = self.__gitlab_adapter.get_available_groups()
+        self.__available_groups_dict = {i+1: groups[i] for i in range(0, len(groups))}
+
+    def __print_available_groups_dict(self):
+        for index, value in self.__available_groups_dict.items():
+            print(f'{index} {value.name} {value.url}')
